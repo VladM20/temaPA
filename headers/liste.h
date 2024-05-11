392031d995d997e7aa7fae477344cbc32df74ff7
Vlad Moisanu <vlad.moisanu@gmail.com>
2024-05-11 22:14:28 +0300

    Reorganizat headere

--- headers/liste.h
+++ headers/liste.h
@@ -1,27 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct Player
-{
-    char *firstName;
-    char *secondName;
-    int points;
-} Player;
-
-typedef struct Member
-{
-    Player player;
-    struct Member *next;
-} Member;
-
-typedef struct Team
-{
-    int numberOfPlayers;
-    char *teamName;
-    Member *members;
-    struct Team *next;
-} Team;
+#include "input.h"
 
 Team *createTeam();
 void addTeam(Team**,Team*);
--- main.c
+++ main.c
@@ -1,18 +1,5 @@
 #include "headers/liste.h"
 
-void readTeamName(FILE *input,char *dest,int n)
-{
-    char c;
-    for(int i=0;i<n;i++)
-    {
-        c=fgetc(input);
-        if(c=='\n')
-            break;
-        dest[i]=c;
-    }
-    dest[n]='\0';
-}
-
 int main(int argc,char* argv[])
 {
     FILE* f=fopen(argv[1],"rt");
