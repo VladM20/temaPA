f4032bc22d01a28f5de449f18cedea434c94ff4e
Vlad Moisanu <vlad.moisanu@gmail.com>
2024-05-13 18:44:51 +0300

    Reorganizat headere

--- headers/liste.h
+++ headers/liste.h
@@ -1,4 +1,27 @@
-#include "input.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH 100
+
+typedef struct Player
+{
+    char *firstName;
+    char *secondName;
+    int points;
+} Player;
+
+typedef struct Team
+{
+    int numberOfPlayers;
+    char *teamName;
+    Player player[];
+    struct Team *next;
+} Team;
+
+void readTeamName(FILE*,char*,int);
+Team *readTeam(FILE*);
+void readMember(FILE*,Member**);
 
 Team *createTeam();
 void addTeam(Team**,Team*);
